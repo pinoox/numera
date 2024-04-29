93782de705534edcc43449b9fe4f3f1b93a7c479
yoosefap <yoosefalipour@gmail.com>
2024-04-29 14:05:12 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Pino\Numera;
 
-echo Numera::init('en')->setCamelCase(true)->convertNumberToWords('12,254,128');
+echo Numera::init('en')->setCamelCase(true)->convertToWords('12,254,128');
